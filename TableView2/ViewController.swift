1a13ace8fe2be4e71032f640133acc5f18befc16
gaeulLee <rhakq23@gmail.com>
2024-03-09 11:37:29 +0900

    Initial Commit

---
+++ TableView2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView2
+//
+//  Created by 이가을 on 3/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
